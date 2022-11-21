88cf3c72d7059eb819b0d01fd4160e4c795ce468
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2022-11-21 20:09:19 +0000

    Added Gropping

---
+++ ASP_RazorContoso/Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP_RazorContoso.Models.SchoolViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
+
+        public int StudentCount { get; set; }
+    }
+}
+
